26f5ee1ab4ff25f166e0c2ca98d982cce0e092fe
Aleks Ivanov <aleks4o@Alekss-MacBook-Pro.local>
2023-08-02 00:04:58 +0300

    fixed navigation animation

--- WebProject/src/Components/Navigation/NavigationDesktop/NavigationDesktop.jsx
+++ WebProject/src/Components/Navigation/NavigationDesktop/NavigationDesktop.jsx
@@ -4,12 +4,12 @@ import logo from "./logo.png";
 
 // eslint-disable-next-line react/prop-types
 const CategoryLink = ({ category, description, onClick }) => (
-  <div
+  <li
     className="category_option_desk"
     onClick={() => onClick(category, description)}
   >
       <a>{category.toUpperCase()}</a>
-  </div>
+  </li>
 );
 
 // eslint-disable-next-line react/prop-types
@@ -24,12 +24,14 @@ export const NavigationDesktop = ({ onCategoryClick }) => {
         <img src={logo} alt="" />
       </div>
       <div className="category_desk">
+        <ul>
         <CategoryLink onClick={handleCategoryClick} category="shoes" description = "The shoe category refers to a wide range of footwear products designed to be worn on the feet for various purposes. Shoes are essential accessories that provide protection, support, and comfort during daily activities and special occasions. They come in diverse styles, designs, and materials, catering to different needs and fashion preferences."/>
         <CategoryLink onClick={handleCategoryClick} category="bags" description = "Description for bags"/>
         <CategoryLink onClick={handleCategoryClick} category="shirts" description = "Description for shirts"/>
         <CategoryLink onClick={handleCategoryClick} category="shorts" description = "Description for shorts"/>
         <CategoryLink onClick={handleCategoryClick} category="dresses" description = "Description for dresses"/>
         <CategoryLink onClick={handleCategoryClick} category="blazers" description = "Description for blazers"/>
+        </ul> 
       </div>
     </div>
   );
